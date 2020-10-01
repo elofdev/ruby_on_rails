2746ff1667224b9507f6fa1f18554fb736e59bcb
elof <ersijunior@gmail.com>
2020-09-30 21:19:45 -0300

    Integraçao com o Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@jquery").start()
+require("@jquery_ujs").start()
 require("@rails/ujs").start()
+require("@bootstrap-sprockets").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+//require("@rails/activestorage").start()
+//require("channels")
+
+//= require jquery
+//= require bootstrap-sprockets
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to:"static_pages#index" # "nome_controller" "#" "nome_aaction"
+  root to:"static_pages#index"
+  # "nome_controller" "#" "nome_action"
 end
